bae300d95849c389b4c36872aa0c642c637ee5d1
regalejandro <abc.regalejandro@gmail.com>
2025-09-22 17:29:21 -0700

    LCD files added

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,23 @@
 
+#include "src/lcd/lcd.h"
 #include "src/extras/tunes.h"
 
-#define TEST_PIN 2
+#define TUNE_PIN 3
+
+LiquidCrystal lcd(RS, E, DB0, DB1, DB2, DB3, DB4, DB5, DB6, DB7);
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(TEST_PIN, OUTPUT);
+  pinMode(TUNE_PIN, OUTPUT);
+
+  lcd.begin(16,1);
+  lcd.print("Hello, World!");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  play_PvZ_theme(TEST_PIN);
+  play_PvZ_theme(TUNE_PIN);
 }
 
 
---
+++ main/src/lcd/lcd.h
@@ -0,0 +1,26 @@
+
+
+
+
+#ifndef LCD_H
+#define LCD_H
+
+#include <Arduino.h>
+#include <stdint.h>
+
+#include <LiquidCrystal.h>
+
+#define RS 52
+#define E 50
+#define DB0 53
+#define DB1 51
+#define DB2 49
+#define DB3 47
+#define DB4 45
+#define DB5 43
+#define DB6 41
+#define DB7 39
+
+
+
+#endif	// LCD_H
